48580a02e074de76291f299b47f4e2129ed3082a
Piyush <meenapiyush23@gmail.com>
2026-03-24 23:48:35 +0530

    Update func.py

--- devgagan/core/func.py
+++ devgagan/core/func.py
@@ -22,6 +22,9 @@ import cv2
 from pyrogram.errors import FloodWait, InviteHashInvalid, InviteHashExpired, UserAlreadyParticipant, UserNotParticipant
 from datetime import datetime as dt
 import asyncio, subprocess, re, os, time
+
+force_stop_flags = {}
+
 async def chk_user(message, user_id):
     user = await premium_users()
     if user_id in user or user_id in OWNER_ID:
@@ -89,6 +92,8 @@ PROGRESS_BAR = """\n
 ╰─────────────────────╯
 """
 async def progress_bar(current, total, ud_type, message, start):
+    if hasattr(message, "chat") and force_stop_flags.get(message.chat.id):
+        raise Exception("Force stop requested")
 
     now = time.time()
     diff = now - start
@@ -232,6 +237,9 @@ async def screenshot(video, duration, sender):
         None  
 last_update_time = time.time()
 async def progress_callback(current, total, progress_message):
+    if hasattr(progress_message, "chat") and force_stop_flags.get(progress_message.chat.id):
+        raise Exception("Force stop requested")
+        
     percent = (current / total) * 100
     global last_update_time
     current_time = time.time()
@@ -255,6 +263,8 @@ async def progress_callback(current, total, progress_message):
 
         last_update_time = current_time
 async def prog_bar(current, total, ud_type, message, start):
+    if hasattr(message, "chat") and force_stop_flags.get(message.chat.id):
+        raise Exception("Force stop requested")
 
     now = time.time()
     diff = now - start
